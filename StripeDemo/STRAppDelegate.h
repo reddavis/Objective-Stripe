bdc0f1d63e8b94189c16a6c4046f232134a4a9d2
Red <red@riothq.com>
2012-08-25 11:28:37 +0100

    Initial Commit

---
+++ StripeDemo/STRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STRAppDelegate.h
+//  StripeDemo
+//
+//  Created by Red Davis on 25/08/2012.
+//  Copyright (c) 2012 Red Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StripeDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StripeDemo
+//
+//  Created by Red Davis on 25/08/2012.
+//  Copyright (c) 2012 Red Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STRAppDelegate class]));
+    }
+}
